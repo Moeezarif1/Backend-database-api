d541625cf44f512216ea6610a25085c042c448ec
Moeezarif1 <Moeezarif340@gmail.com>
2024-01-24 18:43:53 +0500

    Connection Pooling

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,21 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/test_task"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    poolclass=QueuePool,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800,
+)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def get_database_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
